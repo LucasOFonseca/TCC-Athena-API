7d7d133566600c8883cf8db5398b5b950cc2bcf2
Lucas Fonseca <lucasfonsecaeditor@gmail.com>
2023-04-15 02:48:27 -0300

    fix: error handler

--- src/infra/http/routes/index.ts
+++ src/infra/http/routes/index.ts
@@ -1,10 +1,7 @@
 import { Router } from 'express';
-import { handleSuccessMessage } from '../middlewares';
 import { disciplineRoutes } from './discipline.routes';
 
 const router = Router();
-
-router.all('*', handleSuccessMessage);
 
 router.use('/discipline', disciplineRoutes);
 
--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,6 +1,6 @@
 import cors from 'cors';
 import 'dotenv/config';
-import express, { Request, Response } from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
 import { AppError } from './errors';
 import { router } from './routes';
@@ -11,7 +11,7 @@ app.use(cors());
 app.use(express.json());
 app.use(router);
 
-app.use((err: Error, req: Request, res: Response) => {
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.removeHeader('successMessage');
 
   if (err instanceof AppError) {
