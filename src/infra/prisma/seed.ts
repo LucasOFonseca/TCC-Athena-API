6845ac5e5275f0f121a93fbb0eb3766133e87d05
Lucas Fonseca <lucasfonsecaeditor@gmail.com>
2025-04-01 13:56:29 -0300

    feat: update db initialization

--- src/infra/prisma/seed.ts
+++ src/infra/prisma/seed.ts
@@ -1,7 +1,8 @@
-import { Shift } from '../../models/dtos';
+import bcrypt from 'bcrypt';
+import { EmployeeRole, Shift } from '../../models/dtos';
 import { prismaClient } from '.';
 
-async function seed() {
+async function createShifts() {
   const morning = await prismaClient.shift.findFirst({
     where: {
       shift: Shift.morning,
@@ -41,6 +42,52 @@ async function seed() {
       },
     });
   }
+}
+
+async function createAdmin() {
+  const admin = await prismaClient.employee.findUnique({
+    where: {
+      email: 'admin@admin',
+    },
+  });
+
+  if (!admin) {
+    const hashPassword = await bcrypt.hash(
+      'admin',
+      Number(process.env.BCRYPT_SALT)
+    );
+
+    await prismaClient.employee.create({
+      data: {
+        name: 'Admin',
+        cpf: '00000000000',
+        birthdate: new Date().toISOString(),
+        phoneNumber: '00000000000',
+        email: 'admin@admin',
+        password: hashPassword,
+        roles: {
+          create: {
+            role: EmployeeRole.principal,
+          },
+        },
+        address: {
+          create: {
+            street: 'Rua',
+            number: '123',
+            neighborhood: 'Bairro',
+            city: 'Cidade',
+            state: 'UF',
+            cep: '00000000',
+          },
+        },
+      },
+    });
+  }
+}
+
+async function seed() {
+  await createAdmin();
+  await createShifts();
 }
 
 seed();
